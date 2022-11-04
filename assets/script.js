0346456b569517a5432ce5ec2c52ce8f8d093de6
Erin <ejagneaux@gmail.com>
2022-11-03 21:57:58 -0600

    structure html and css bootstrap

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+  $(function (){" "}
+  {$("#collapseFour").collapse({
+    toggle: false,
+  })}
+  ); $(function () {$("#collapseTwo").collapse("show")}); $(function (){" "}
+  {$("#collapseThree").collapse("toggle")}); $(function (){" "}
+  {$("#collapseOne").collapse("hide")});
+</script>;
