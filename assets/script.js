ef4dbfb3e1542c2e8920cbf59e8b46d3eb74a5ff
IsisRobbs <isisrobbs21@gmail.com>
2022-12-28 22:01:10 -0600

    Add new js file for task class

--- assets/script.js
+++ assets/script.js
@@ -1,35 +1,35 @@
-(function() {
+const taskForm = (function() {
   'use strict';
   window.addEventListener('load', function() {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
-    var taskName = document.getElementById('validationFormTaskNameInput')
-    var firstName = document.getElementById('validationFirstName')
-    var lastName = document.getElementById('validationLastName')
-    var submit = document.getElementById('submitbutton')
     // Loop over them and prevent submission
     var validation = Array.prototype.filter.call(forms, function(form) {
-      form.addEventListener('submit', function(event) {
-        if (form.checkValidity() === false) {
+      form.addEventListener(
+        'submit', 
+        function(event) {
+    var taskName = document.getElementById('validationFormTaskNameInput');
+    var taskDescription = document.getElementById('validationDescription');
+    var firstName = document.getElementById('validationFirstName');
+    var lastName = document.getElementById('validationLastName');
+    var dueDate = document.getElementById('validationDate');
+    var submit = document.getElementById('submitButton');
+    var taskNameError = document.getElementById('taskNameError');
+        
+    if (form.checkValidity() === false) {
           event.preventDefault();
-          event.stopPropagation();} 
-          form.classList.add('was-validated');
-        }) 
-        taskName.addEventListener('submit', function(e) {
-          if (taskName.length > 30){
-            e.preventDefault();
-            e.stopPropagation();
-            return 'Task name can only be 50 characters long!';}
-        })
-      }, false);
-      
-  }, false);
+          event.stopPropagation();
+        }   
+        form.classList.add('was-validated');
+        
+        var taskNameChar = taskName.value.length;
+        if(taskNameChar > 30) {
+          event.preventDefault();
+          event.stopPropagation();
+          console.log('Task name can be no longer than 30 characters long!')
+        }
+       });
+      }, false);   
+   }, false);
 })();
-
-
     
-class AddTask {
-  constructor(taskName, taskDescription, firstName, lastName, dueDate, progress, submitButton)
-
-};
-
---
+++ assets/tasks.js
@@ -0,0 +1,17 @@
+class AddTask {
+    constructor(taskName, taskDescription, firstName, lastName, dueDate, progress, submitButton){
+      this.taskName = taskName,
+      this.taskDescription = taskDescription,
+      this.firstName = firstName,
+      this.lastName = lastName,
+      this.dueDate = dueDate,
+      this.progress = progress,
+      this.submitButton = submitButton
+    }
+    }
+    
+    class TaskManager {
+      constructor(){
+        
+      }
+    }
